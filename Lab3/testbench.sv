4aaf98c6261241d7e4acb2efb0787012241c41d1
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-02-06 20:47:35 -0600

    updating test

--- Lab3/testbench.sv
+++ Lab3/testbench.sv
@@ -3,3 +3,35 @@ module testbench();
 timeunit 10ns;
 
 timeprecision 1ns;
+
+input logic Clk; 
+input logic Reset;
+input logic LoadB;
+input logic Run;
+input logic[15:0] SW;
+
+//all outputs are registered
+output logic CO;
+output logic[15:0] Sum;
+output logic[6:0] Ahex0;
+output logic[6:0] Ahex1;
+output logic[6:0] Ahex2;
+output logic[6:0] Ahex3;
+output logic[6:0] Bhex0;
+output logic[6:0] Bhex1;
+output logic[6:0] Bhex2;
+output logic[6:0] Bhex3;
+
+always begin : CLOCK_GENERATION
+
+#1 Clk = ~Clk; //Starts the clock
+
+end
+
+initial begin
+    Clk = 0;
+end
+
+
+
+endmodule
